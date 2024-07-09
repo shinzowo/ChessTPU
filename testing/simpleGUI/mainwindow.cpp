b4a4199d96761a19ec02e7a6bea93d6419044b83
dok23 <dok23@tpu.ru>
2024-07-09 21:02:55 +0700

    Add new responsive interface prototype that is to be implemented in the final version.

--- testing/simpleGUI/mainwindow.cpp
+++ testing/simpleGUI/mainwindow.cpp
@@ -14,21 +14,7 @@ MainWindow::MainWindow(QWidget *parent)
     buttonStyle=QString("QPushButton{background-color: %1; color: %2; border-style:outset;"
                         "}"
                         "QPushButton:hover{background-color: %3}").arg(baseColorButton).arg(textColor).arg(hoverColor);
-    ui->label->setStyleSheet("QLabel{color:#ffffff;}");
-    ui->label_2->setStyleSheet("QLabel{color:#ffffff;}");
-    ui->label_4->setStyleSheet("QLabel{color:#ffffff;}");
-    ui->ClassicGameButton->setStyleSheet(buttonStyle);
-    ui->FischerChessButton->setStyleSheet(buttonStyle);
-    ui->ThreeCheckButton->setStyleSheet(buttonStyle);
-    ui->PuzzleButton->setStyleSheet(buttonStyle);
-    ui->QuitButton->setStyleSheet(buttonStyle);
-    ui->BotButton->setStyleSheet(buttonStyle);
-    ui->TwoPlayersButton->setStyleSheet(buttonStyle);
-    ui->BackButton->setStyleSheet(buttonStyle);
-    ui->WhiteButton->setStyleSheet(buttonStyle);
-    ui->RandomButton->setStyleSheet(buttonStyle);
-    ui->BlackButton->setStyleSheet(buttonStyle);
-    ui->BackToChooseUIButton->setStyleSheet(buttonStyle);
+
 }
 
 MainWindow::~MainWindow()
@@ -37,74 +23,74 @@ MainWindow::~MainWindow()
 }
 
 
-void MainWindow::on_ClassicGameButton_clicked()
-{
-    ui->stackedWidget->setCurrentIndex(choose_ui);
-}
+// void MainWindow::on_ClassicGameButton_clicked()
+// {
+//     ui->stackedWidget->setCurrentIndex(choose_ui);
+// }
 
 
-void MainWindow::on_FischerChessButton_clicked()
-{
-    ui->stackedWidget->setCurrentIndex(choose_ui);
-}
+// void MainWindow::on_FischerChessButton_clicked()
+// {
+//     ui->stackedWidget->setCurrentIndex(choose_ui);
+// }
 
 
-void MainWindow::on_ThreeCheckButton_clicked()
-{
-    ui->stackedWidget->setCurrentIndex(choose_ui);
-}
+// void MainWindow::on_ThreeCheckButton_clicked()
+// {
+//     ui->stackedWidget->setCurrentIndex(choose_ui);
+// }
 
 
-void MainWindow::on_PuzzleButton_clicked()
-{
+// void MainWindow::on_PuzzleButton_clicked()
+// {
 
-}
-
-
-void MainWindow::on_QuitButton_clicked()
-{
-    this->close();
-}
+// }
 
 
-void MainWindow::on_TwoPlayersButton_clicked()
-{
-
-}
-
-
-void MainWindow::on_BotButton_clicked()
-{
-    ui->stackedWidget->setCurrentIndex(side_ui);
-}
+// void MainWindow::on_QuitButton_clicked()
+// {
+//     this->close();
+// }
 
 
-void MainWindow::on_BackButton_clicked()
-{
-    ui->stackedWidget->setCurrentIndex(base_ui);
-}
+// void MainWindow::on_TwoPlayersButton_clicked()
+// {
+
+// }
 
 
-void MainWindow::on_WhiteButton_clicked()
-{
-
-}
-
-
-void MainWindow::on_RandomButton_clicked()
-{
-
-}
+// void MainWindow::on_BotButton_clicked()
+// {
+//     ui->stackedWidget->setCurrentIndex(side_ui);
+// }
 
 
-void MainWindow::on_BlackButton_clicked()
-{
+// void MainWindow::on_BackButton_clicked()
+// {
+//     ui->stackedWidget->setCurrentIndex(base_ui);
+// }
 
-}
+
+// void MainWindow::on_WhiteButton_clicked()
+// {
+
+// }
 
 
-void MainWindow::on_BackToChooseUIButton_clicked()
-{
-     ui->stackedWidget->setCurrentIndex(choose_ui);
-}
+// void MainWindow::on_RandomButton_clicked()
+// {
+
+// }
+
+
+// void MainWindow::on_BlackButton_clicked()
+// {
+
+// }
+
+
+// void MainWindow::on_BackToChooseUIButton_clicked()
+// {
+//      ui->stackedWidget->setCurrentIndex(choose_ui);
+// }
 
--- testing/simpleGUI/mainwindow.h
+++ testing/simpleGUI/mainwindow.h
@@ -17,29 +17,29 @@ public:
     ~MainWindow();
 
 private slots:
-    void on_ClassicGameButton_clicked();
+    // void on_ClassicGameButton_clicked();
 
-    void on_FischerChessButton_clicked();
+    // void on_FischerChessButton_clicked();
 
-    void on_ThreeCheckButton_clicked();
+    // void on_ThreeCheckButton_clicked();
 
-    void on_PuzzleButton_clicked();
+    // void on_PuzzleButton_clicked();
 
-    void on_QuitButton_clicked();
+    // void on_QuitButton_clicked();
 
-    void on_TwoPlayersButton_clicked();
+    // void on_TwoPlayersButton_clicked();
 
-    void on_BotButton_clicked();
+    // void on_BotButton_clicked();
 
-    void on_BackButton_clicked();
+    // void on_BackButton_clicked();
 
-    void on_WhiteButton_clicked();
+    // void on_WhiteButton_clicked();
 
-    void on_RandomButton_clicked();
+    // void on_RandomButton_clicked();
 
-    void on_BlackButton_clicked();
+    // void on_BlackButton_clicked();
 
-    void on_BackToChooseUIButton_clicked();
+    // void on_BackToChooseUIButton_clicked();
 
 private:
     Ui::MainWindow *ui;
